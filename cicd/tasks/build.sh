61a7b2e6632fd769248e7171a08a3ebd99656f8f
brandon <bsm2112@yahoo.com>
2025-05-25 14:18:24 -0500

    Update script

--- cicd/tasks/build.sh
+++ cicd/tasks/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+apk add --no-cache curl
+apk add --no-cache python3
+
 curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
 python3 get-platformio.py
 export PATH=$PATH:$HOME/.local/bin
