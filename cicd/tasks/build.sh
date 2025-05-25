df0cdef02b6b4c22ee9fabacd74ed05f09bf0a07
brandon <bsm2112@yahoo.com>
2025-05-25 14:24:14 -0500

    Update script

--- cicd/tasks/build.sh
+++ cicd/tasks/build.sh
@@ -7,6 +7,11 @@ curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/pla
 python3 get-platformio.py
 export PATH=$PATH:$HOME/.local/bin
 
+mkdir -p /usr/local/bin
+ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
+ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
+ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
+
 cd repo
 
 ./scripts/build-library.sh
