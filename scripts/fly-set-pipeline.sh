380d587d8ccc9ef250d3f1066cae65fedf7a96f0
brandon <bsm2112@yahoo.com>
2025-05-31 23:32:13 -0500

    update scripts

--- scripts/fly-set-pipeline.sh
+++ scripts/fly-set-pipeline.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -e
 
-yes | fly -t rgblib set-pipeline -c cicd/deploy-pipeline.yml -p rgblib-deploy -l cicd/deploy-variables.yml
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <variables.yml>"
+  exit 1
+fi
+
+VARIABLES_YML="$1"
+
+
+yes | fly -t rgblib set-pipeline -c cicd/deploy-pipeline.yml -p rgblib-deploy -l "${VARIABLES_YML}"
